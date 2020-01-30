06417c578f8fa56b6873f4565c348c0ae835ab39
91070160 <91070160@g.mvusd.net>
2020-01-30 13:25:46 -0800

    Initial commit

---
+++ src/Advice.java
@@ -0,0 +1,10 @@
+public class Advice extends Thought
+{
+    public void message ()
+    {
+        System.out.println("think before you speak.");
+        super.message();
+    }
+}
+//********************ALEJANDRA WAS HERE*************************//
+
---
+++ src/Thought.java
@@ -0,0 +1,10 @@
+public class Thought
+{
+        public void message()
+        {
+            System.out.println("Im thinking, Im Thinking.");
+            System.out.println();
+        }
+
+}
+//********************ALEJANDRA WAS HERE*************************//
